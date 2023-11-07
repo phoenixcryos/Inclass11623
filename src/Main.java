ca7a36df4fd048faa1a5a17ddaa897e863169609
Richard Sharp <rsharp32@student.cccs.edu>
2023-11-06 17:52:34 -0700

    initial

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.list;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Zebra> zebras = new ArrayList<>();
+        zebras.add(new Zebra( numberOfStripes: 9));
+        zebras.add(new Zebra( numberOfStripes: 58));
+        zebras.add(new Zebra( numberOfStripes: 15));
+
+        System.out.println(getBiggestZebraByLoop(zebras).getNumberOfStripes());
+    }
+
+    public static Zebra
+}
---
+++ src/Zebra.java
@@ -0,0 +1,3 @@
+public class Zebra {
+
+private int }
